2f11e611f0b1fba93566dd5d57d6334df547a70d
Cleber Souza <clebersouzaoliveira5@gmail.com>
2021-12-31 19:39:43 -0300

    Adicionar arquivos de projeto.

---
+++ projeto-calculadora/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace projeto_calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int num1, num2, resultado = 0;
+            string operador;
+           
+            Console.WriteLine(">>>>>>>>CALCULADORA!<<<<<<<<<\n\n\n");
+
+            Console.WriteLine("DIGITE O PRIMEIRO VALOR: ");
+            num1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE OPERADOR>>> ");
+            operador = Console.ReadLine();
+
+
+            Console.WriteLine("DIGITE O SEGUNDO VALOR: ");
+            num2 = int.Parse(Console.ReadLine());
+
+
+            switch(operador)
+            {
+                case "+":
+             resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            Console.WriteLine("{0} {1} {2} = {3}", num1, operador, num2, resultado);
+
+        }
+    }
+}
